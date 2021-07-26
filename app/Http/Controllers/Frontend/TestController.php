4b2d6ec68b4f0b958ac86b486c7ba0c496855175
Your Name <you@example.com>
2021-07-27 00:46:32 +0700

    test

--- app/Http/Controllers/Frontend/TestController.php
+++ app/Http/Controllers/Frontend/TestController.php
@@ -11,7 +11,23 @@ class TestController extends Controller {
 
 
     public function index(Request $request) {  
-        return view('frontend/test/index');
+    	$test = DB::table('test')->where('status',1)->first();
+        $section = DB::table('section')->where('test_id',$test->id)->get();
+        $questions = DB::table('question')->join('quizz','quizz.id','=','question.quizz_id')->get()->groupBy('title');
+        foreach ($questions as $key => $question) {
+        	foreach ($question as $key1 => $quest) {
+        		if($quest->question_type == 2){
+        			 $index = str_replace("[input]", "<input type='text'>", $quest->question);
+        			 $question[$key1]->question = $index;
+        		}
+        		elseif($quest->question_type == 5){
+                     $index = str_replace("[", "<a href='#'>", $quest->question);
+                     $index2 = str_replace("]", "</a>", $index);
+                     $question[$key1]->question = $index2;
+        		}
+        	}
+        }
+        return view('frontend/test/index',compact('questions'));
     }
 
    
--- resources/views/frontend/test/index.blade.php
+++ resources/views/frontend/test/index.blade.php
@@ -3,7 +3,64 @@
 
 
 
-
+<div class="container">
+	
+	    @foreach($questions as $key => $question)
+        <h5>{{$key}}</h5>
+        <div class="row" style="margin-bottom: 50px;">
+        	<div class="card">
+	           <div class="card-body">
+	        @foreach($question as $key1=> $quest)
+	        
+	           @if($quest->question_type == 1)
+                    	<div class="col-md-12">Question {{++$key1}}: {!!$quest->question!!}</div>
+                         <div class="row">
+       
+                         @foreach( explode(',',$quest->list_answer) as $list)
+                              <div class="form-check col-md-3">
+							    <input type="radio" name="{{$key1}}" class="form-check-input" id="exampleCheck1">
+							    <label class="form-check-label" for="exampleCheck1">{{$list}}</label>
+							  </div>
+                         @endforeach
+                         </div>
+	           @elseif($quest->question_type == 2)
+                    <div class="col-md-12">Question {{++$key1}}: {!!$quest->question!!}</div>
+                         
+	           @elseif($quest->question_type == 3)
+                    <div class="col-md-12">Question {{++$key1}}: 
+                    	<select>
+                    		<option>True</option>
+                    		<option>False</option>
+                    		<option>Not Given</option>
+                    	</select>
+                    	{{$quest->question}}
+                    </div>
+                         
+	            @elseif($quest->question_type == 5)
+                    <div class="col-md-12">Question {{++$key1}}: {!!$quest->question!!}</div>
+                      
+                         
+	           @elseif($quest->question_type == 4)
+                    <div class="row">
+                         Question {{++$key1}}: 
+                         @foreach( explode(',',$quest->list_answer) as $list)
+                              <div class="form-check col-md-3">
+							    <input name="{{$key1}}" type="radio" class="form-check-input" id="exampleCheck1">
+							    <label class="form-check-label" for="exampleCheck1">{{$list}}</label>
+							  </div>
+                         @endforeach
+                         </div>
+                      
+                         
+	           @endif
+	 
+	        @endforeach
+	                    </div>
+	</div>
+	    </div>    
+	    @endforeach
+	
+</div>
 
 
 

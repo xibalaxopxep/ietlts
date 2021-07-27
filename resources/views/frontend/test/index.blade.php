bd8d385d0b39e30f702892eb462436a7e676fbbc
SERVER <info@kalzen.com>
2021-07-27 14:51:09 +0700

    test

--- resources/views/frontend/test/index.blade.php
+++ resources/views/frontend/test/index.blade.php
@@ -1,18 +1,44 @@
-
-
-
-<div class="container">
-	
+<!doctype html>
+  <html lang="vi">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="generator" content="">
+    <title>Pasal</title>
+    <link rel="canonical" href="">
+    <!-- Bootstrap core CSS -->
+    <link href="{{asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets_pasal/css/pasal.css?v=1.2')}}" rel="stylesheet">
+    <link href="{{asset('assets_pasal/css/test.css?v=1.9')}}" rel="stylesheet">
+      <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  </head>
+  <body id="test">
+  <div class="underlay">
+  </div>
+    <div class="container" id="content">
+    <div class="row">
+        <div class="logo-holder">
+            <img class="logo" src="{{asset('assets_pasal/img/logo.png')}}" alt="logo" />
+        </div>
+        </div>
+        <div class="content-test">
+            <div class="question-list">
+            <div class="part-name w-100"><a class="box-shadow">IV. LISTENING</a></div>
+            <div id="waveform"></div>
 	    @foreach($questions as $key => $question)
+        
         <h5>{{$key}}</h5>
-        <div class="row" style="margin-bottom: 50px;">
-        	<div class="card">
+        <div class="list" style="margin-bottom: 50px;">
+        
+        	<div class="card box-shadow">
 	           <div class="card-body">
 	        @foreach($question as $key1=> $quest)
 	        
 	           @if($quest->question_type == 1)
                     	<div class="col-md-12">Question {{++$key1}}: {!!$quest->question!!}</div>
-                         <div class="row">
+                         
        
                          @foreach( explode(',',$quest->list_answer) as $list)
                               <div class="form-check col-md-3">
@@ -20,7 +46,7 @@
 							    <label class="form-check-label" for="exampleCheck1">{{$list}}</label>
 							  </div>
                          @endforeach
-                         </div>
+                        
 	           @elseif($quest->question_type == 2)
                     <div class="col-md-12">Question {{++$key1}}: {!!$quest->question!!}</div>
                          
@@ -39,7 +65,6 @@
                       
                          
 	           @elseif($quest->question_type == 4)
-                    <div class="row">
                          Question {{++$key1}}: 
                          @foreach( explode(',',$quest->list_answer) as $list)
                               <div class="form-check col-md-3">
@@ -47,7 +72,7 @@
 							    <label class="form-check-label" for="exampleCheck1">{{$list}}</label>
 							  </div>
                          @endforeach
-                         </div>
+
                       
                          
 	           @endif
@@ -56,9 +81,29 @@
 	                    </div>
 	</div>
 	    </div>    
+        	
 	    @endforeach
-	
+        </div>
 </div>
+</div>
+    </body>
+    <footer>
+ 
 
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> 
+      <!-- Magnific Popup core JS file -->
+      <script src="{{asset('assets_pasal/magnific-popup/jquery.magnific-popup.js')}}"></script>
+      <script src="{{asset('assets_pasal/js/style.js')}}"></script>
+      <script src="https://unpkg.com/wavesurfer.js"></script>
+      <script>
+      var wavesurfer = WaveSurfer.create({
+        container: '#waveform',
+        waveColor: 'violet',
+        progressColor: 'purple'
+    });
+    wavesurfer.load('example.wav');
+      </script>
+    </footer>
+    </html>
 
 

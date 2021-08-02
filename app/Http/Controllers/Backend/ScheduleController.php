ff76934ac0c23699055d78c0c4cb19158665505a
Your Name <you@example.com>
2021-08-02 10:39:47 +0700

    dat

--- app/Http/Controllers/Backend/ScheduleController.php
+++ app/Http/Controllers/Backend/ScheduleController.php
@@ -20,14 +20,14 @@ class ScheduleController extends Controller {
     }
 
 
-    public function create($type) {
+    public function create() {
         $count_ordering =DB::table('schedule')->count();
         $courses = DB::table('course')->get();
         $address = DB::table('contact_address')->get();
         return view('backend/schedule/create', compact('count_ordering','courses', 'address'));
     }
 
-    public function store(Request $request,$type) {
+    public function store(Request $request) {
         $schedule = new Schedule();
         $input = $request->all();
         $validator = \Validator::make($input, $schedule->validateCreate());
--- resources/views/frontend/layouts/__header.blade.php
+++ resources/views/frontend/layouts/__header.blade.php
@@ -16,9 +16,7 @@
               <li class="nav-item"><a href="{{url($mn->link)}}" class="nav-link" aria-current="page">{{$mn->title}}</a></li>
 
               @endforeach
-              <ul>
-                <li>1</li>
-              </ul>
+              
             </ul>
           </header>
 
